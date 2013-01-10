992daa6333f7ec40e8df6facce0457e54a49ae61
枫叶饭团 <maplebeats@gmail.com>
2013-01-10 19:10:38 +0800

    add get title function to bot

--- bot.py
+++ bot.py
@@ -28,17 +28,44 @@ class Bot:
 
     def __init__(self):
         self.simi_init()
-        self.link = re.compile(r'[a-z]')
 
+    @staticmethod
     def gettitle(url):
-        re = request.urlopen(url).read(1024)
-        pass
+        """
+        need rewrite
+        """
+        title = re.compile(b'<title>(.*)</title>')
+        r = request.urlopen(url).read(1024)
+        if r.find(b'utf-8') != -1:
+            de = 'utf-8'
+        else:
+            de = 'gbk'
+        t = title.findall(r)
+        if t:
+            t = t[0]
+            try:
+                return t.decode(de)
+            except:
+                if de == 'gbk':
+                    return t.decode('utf-8')
+                else:
+                    return t.decode('gbk')
+        else:
+            return "this page didn't have title"
 
     def reply(self, req):
-        if req.find('%') == -1:
-            return self.simi_bot(req) or self.hito_bot()
+        link = re.compile(r'(?:http[s]?://)?.*\.\w{2,5}[\w/]*', re.I)
+        l = link.findall(req)
+        if l:
+            u = []
+            for i in l:
+                if i.find('http') == -1:
+                    i = 'http://' + i
+                u.append(Bot.gettitle(i))
+            return u[0].strip()
         else:
-            return self.hito_bot()
+            return False
+            #return self.simi_bot(req) or self.hito_bot()
 
     def simi_init(self):
         simi_Jar = cookiejar.CookieJar()
@@ -79,13 +106,15 @@ class Qbot(Webqq):
     def grouphandler(self, data):
         content = data['content'][1]
         re = self.bot.reply(content)
-        self.send_group_msg(data['from_uin'], re)
+        if re:
+            self.send_group_msg(data['from_uin'], re)
         logger.info("IN:%s\nreply group:%s"%(content, re))
 
     def userhandler(self, data):
         content = data['content'][1]
         re = self.bot.reply(content)
-        self.send_user_msg(data['from_uin'], re)
+        if re:
+            self.send_user_msg(data['from_uin'], re)
         logger.info("IN:%s\nreply user:%s"%(content, re))
 
 if __name__ == "__main__":
@@ -93,3 +122,4 @@ if __name__ == "__main__":
     c = qqcfg()
     qq = Qbot(c[0],c[1])
     qq.login()
+    #print(Bot.gettitle('http://www.qq.com'))
